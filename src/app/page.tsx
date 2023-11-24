dd42d26eeb52ddcaf5fe5007022077cca54ec933
Mauricio Rojas <mrojasb2000@gmail.com>
2023-11-24 18:25:40 -0300

    refactor: twitter layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,14 @@ import {BiHomeCircle, BiUser} from 'react-icons/bi'
 import {HiOutlineHashtag} from 'react-icons/hi'
 import {BsBell, BsBookmark, BsTwitter} from 'react-icons/bs'
 import {HiEnvelope} from 'react-icons/hi2'
+import {CiCircleMore} from 'react-icons/ci'
 import Link from 'next/link'
 
 const NAVIGATION_ITEMS = [
+  {
+    title: 'Twitter',
+    icon:BsTwitter
+  },
   {
     title: 'Home',
     icon:BiHomeCircle
@@ -29,29 +34,34 @@ const NAVIGATION_ITEMS = [
     title: 'Profile',
     icon:BiUser,
   },
+  {
+    title: 'More',
+    icon:CiCircleMore,
+  },
+  
 ]
 
 const home = () => {
   return (
     <div className="w-full h-full flex justify-center items-center relative bg-black">
-      <div className="max-w-screen-lg w-full h-full flex relative">
-        <section className="fixed w-72 flex flex-col h-screen space-y-4 my-4">
-          <Link href={"/"} className='p-2 text-2xl'>
-            <BsTwitter />
-          </Link>
+      <div className="max-w-screen-xl w-full h-full flex relative">
+        <section className="fixed w-[275px] flex flex-col h-screen space-y-4 my-4">
+          
           {NAVIGATION_ITEMS.map((item) => (
-              <Link className='hover:bg-white/10 text-2xl transition duration-200 flex items-center justify-start w-fit space-x-2 rounded-3xl p-2' href={`/${item.title.toLowerCase()}`} key={item.title}>
+              <Link className='hover:bg-white/10 text-2xl transition duration-200 flex items-center justify-start w-fit space-x-2 rounded-3xl p-2 px-6' href={`/${item.title.toLowerCase()}`} key={item.title}>
                 <div>
                   <item.icon />
                 </div>
                 <div>
                   {
-                    item.title
+                    item.title !== "Twitter" && <div>{item.title}</div>
                   }
                 </div>
               </Link>
-          ))
-          }
+          ))}
+          <button className='rounded-full m-4 bg-sky-500 px-6 py-4 text-2xl text-center hover:bg-opacity-70 transition duration-200'>
+            Tweet
+          </button>
         </section>
         <main></main>
         <section></section>
